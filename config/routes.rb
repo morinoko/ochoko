df5e386beb29b3652d8345f53bbb2536c9246355
Felice Forby <forby.1@gmail.com>
2018-11-27 08:00:48 +0900

    enable user signup

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,10 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   
   scope "(:locale)", locale: /en|ja/ do
-    resources :users, only: [:new, :show]
+    resources :users, only: [:create, :show]
+    get '/signup' => "users#new"
+    
     root 'welcome#index'
   end
+
 end
