eda1d7dfa0b86d0c509d42180724189352e7fe78
Felice Forby <forby.1@gmail.com>
2018-11-28 08:34:29 +0900

    User can be found or created by omniauth auth information

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,13 @@ class User < ApplicationRecord
   validates :password, presence: true
   validates :username, presence: true
   validates :email, presence: true, uniqueness: true
+  
+  def self.find_or_create_by_omniauth(auth_hash)
+    self.where(email: auth_hash["info"]["email"]).first_or_create do |user|
+      user.username = auth_hash["info"]["name"]
+      user.password = SecureRandom.hex
+      
+      # may need uid and provider later?
+    end
+  end
 end
