9de0722c2397347b8238065c6bff47f42ac08973
Felice Forby <forby.1@gmail.com>
2018-11-29 07:51:10 +0900

    add basic tests for users and controller

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,24 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should not save user without username" do
+    user = User.new(username: nil, email: "test@gmail.com", password: "test")
+    assert_not user.save
+  end
+  
+  test "should not save user without an email" do
+    user = User.new(username: "John", email: nil, password: "test")
+    assert_not user.save
+  end
+  
+  test "should not save user without an password" do
+    user = User.new(username: "John", email: "test@gmail.com", password: nil)
+    assert_not user.save
+  end
+  
+  test "should not save user if another user has the same email" do
+    john_smith = User.create(username: "John Smith", email: "john@gmail.com", password: "password")
+    john_doe = User.new(username: "John Doe", email: "john@gmail.com", password: "password2")
+    assert_not john_doe.save
+  end
 end
