1a72d84ab5c854e1226bb6cb9a4c2b277e0d567c
Felice Forby <forby.1@gmail.com>
2018-11-29 07:41:43 +0900

    track provider and uid when logging in with facebook

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,9 +8,9 @@ class User < ApplicationRecord
   def self.find_or_create_by_omniauth(auth_hash)
     self.where(email: auth_hash["info"]["email"]).first_or_create do |user|
       user.username = auth_hash["info"]["name"]
+      user.provider = auth_hash["info"]["provider"]
+      user.uid = auth_hash["info"]["uid"]
       user.password = SecureRandom.hex
-      
-      # may need uid and provider later?
     end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_24_075420) do
+ActiveRecord::Schema.define(version: 2018_11_28_223856) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,8 @@ ActiveRecord::Schema.define(version: 2018_11_24_075420) do
     t.string "username"
     t.string "email"
     t.string "password_digest"
+    t.string "provider"
+    t.string "uid"
   end
 
 end
