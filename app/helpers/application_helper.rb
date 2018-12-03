36651685993ef5b15e1cedb94028d79520f242ea
Felice Forby <forby.1@gmail.com>
2018-12-04 08:38:23 +0900

    display helpful error messages for each invalid field in signup form

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
 module ApplicationHelper
+  def error_feedback(object:, attribute:)
+    tag.div class: "invalid-feedback" do
+      object.errors.full_messages_for(attribute.to_sym).first
+    end
+  end
 end
