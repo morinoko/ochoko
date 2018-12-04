932362f9d7e4d00e6bcf93578086403c480d9e44
Felice Forby <forby.1@gmail.com>
2018-12-05 08:45:57 +0900

    start building tasting note form

--- app/controllers/tasting_notes_controller.rb
+++ app/controllers/tasting_notes_controller.rb
@@ -1,2 +1,14 @@
 class TastingNotesController < ApplicationController
+  def index
+  end
+  
+  def new
+    @sakes = Sake.all
+    @user = current_user
+    @tasting_note = @user.tasting_notes.build
+  end
+  
+  def create
+    
+  end
 end
--- app/models/sake.rb
+++ app/models/sake.rb
@@ -1,4 +1,12 @@
 class Sake < ApplicationRecord
   belongs_to :brewery
   has_many :notes
+  
+  def localized_name
+    if I18n.locale == :ja
+      "#{japanese_name} #{type}"
+    else
+      "#{romanized_name} #{type}"
+    end
+  end
 end
