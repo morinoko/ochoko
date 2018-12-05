ae1948f30fa66c2e17cdde86d40541288d883a74
Felice Forby <forby.1@gmail.com>
2018-12-05 21:28:44 +0900

    Add seed data to add every Japanese prefecture to Locations table

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+JpPrefecture::Prefecture.all.each do |pref|
+  Location.where(prefecture_code: pref.code).first_or_create do |location|
+    location.prefecture_code = pref.code
+  end
+end
