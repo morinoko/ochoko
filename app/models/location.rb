b8348d7eaaa21da039cbd9bc553b3821147cf21e
Felice Forby <forby.1@gmail.com>
2018-12-05 21:53:16 +0900

    add note on JpPrefecture methods for data access in location model

--- app/models/location.rb
+++ app/models/location.rb
@@ -11,4 +11,11 @@ class Location < ApplicationRecord
   def japanese_name
     self.prefecture.name
   end
+  
+  # Other JpPrefecture data can be accesssed by these methods:
+  # location.prefecture.code => 13
+  # location.prefecture.name_h => "とうきょうと"
+  # location.prefecture.name_k => "トウキョウト"
+  # location.prefecture.area => "関東"
+  # location.prefecture.type => "都"
 end
