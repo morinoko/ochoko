072f23bca6805c38af16b7f055ce67f560ef4124
Felice Forby <forby.1@gmail.com>
2019-01-05 16:47:08 +0900

    add select options for sake grade

--- app/helpers/sakes_helper.rb
+++ app/helpers/sakes_helper.rb
@@ -1,2 +1,7 @@
 module SakesHelper
+  def sake_grades_for_select
+    language = I18n.locale == :ja ? :japanese : :english
+    
+    Sake::GRADES.map { |key, value| [Sake::GRADES[key][language], key] }
+  end
 end
--- app/models/sake.rb
+++ app/models/sake.rb
@@ -1,12 +1,39 @@
 class Sake < ApplicationRecord
   belongs_to :brewery
   has_many :notes
-  
+
+  GRADES = {
+    :honjozo => { japanese: "本醸造",
+                  english: "Honjōzō" },
+    :tokubetsu_honjozo => { japanese: "特別本醸造",
+                            english: "Tokubetsu Honjōzō" },
+    :ginjo => { japanese: "吟醸",
+                english: "Ginjō" },
+    :daiginjo => { japanese: "大吟醸",
+                    english: "Daiginjō" },
+    :junmai => { japanese: "純米",
+                  english: "Junmai" },
+    :tokubetsu_junmai => { japanese: "特別純米",
+                           english: "Tokubetsu Junmai" },
+    :junmai_ginjo => { japanese: "純米吟醸",
+                       english: "Junmai Ginjō" },
+    :junmai_daiginjo => { japanese: "純米大吟醸",
+                          english: "Junmai Daiginjō" }
+  }
+
   def localized_name
     if I18n.locale == :ja
       "#{japanese_name} #{sake_type}"
     else
       "#{romanized_name} #{sake_type}"
     end
+  end
+
+  def self.grades_english
+    GRADES.map { |key, value| GRADES[key][:english] }
+  end
+
+  def self.grades_japanese
+    GRADES.map { |key, value| GRADES[key][:japanese] }
   end
 end
