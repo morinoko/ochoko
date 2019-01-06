21b8c318f323e0b240d7773d89d2b149eb8d1344
Felice Forby <forby.1@gmail.com>
2019-01-07 06:54:11 +0900

    add sake type to sake form and use strong params

--- app/controllers/sakes_controller.rb
+++ app/controllers/sakes_controller.rb
@@ -8,4 +8,10 @@ class SakesController < ApplicationController
     @breweries = Brewery.all
   end
   
+  private
+  
+  def sake_params
+    params.require(:sake).permit(:japanese_name, :hiragana_name, :romanized_name, :grade, :sake_type, :brewery_id)
+  end
+  
 end
