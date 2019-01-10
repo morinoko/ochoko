fd5ad23d61df7344342ecd06cdaaabe82f77b9ee
Felice Forby <forby.1@gmail.com>
2019-01-11 07:52:24 +0900

    add basic index and show pages for breweries

--- app/controllers/breweries_controller.rb
+++ app/controllers/breweries_controller.rb
@@ -1,2 +1,16 @@
 class BreweriesController < ApplicationController
+  before_action :set_brewery, only: [:show]
+  
+  def index
+    @breweries = Brewery.all
+  end
+  
+  def show
+  end
+  
+  private
+  
+  def set_brewery
+    @brewery = Brewery.find_by(id: params[:id])
+  end
 end
