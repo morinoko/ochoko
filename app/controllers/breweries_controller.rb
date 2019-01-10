ada26e52299bfdc97c890a1757f2866f170a953f
Felice Forby <forby.1@gmail.com>
2019-01-11 08:55:21 +0900

    implement brewery creation and show form errors

--- app/controllers/breweries_controller.rb
+++ app/controllers/breweries_controller.rb
@@ -11,9 +11,25 @@ class BreweriesController < ApplicationController
   
   def new
     @brewery = Brewery.new
+    @locations = Location.all
+  end
+  
+  def create
+    @brewery = Brewery.new(brewery_params)
+    
+    if @brewery.save
+      redirect_to @brewery
+    else
+      @locations = Location.all
+      render :new
+    end
   end
   
   private
+  
+  def brewery_params
+    params.require(:brewery).permit(:japanese_name, :hiragana_name, :romanized_name, :location_id)
+  end
   
   def set_brewery
     @brewery = Brewery.find_by(id: params[:id])
--- app/models/brewery.rb
+++ app/models/brewery.rb
@@ -2,6 +2,11 @@ class Brewery < ApplicationRecord
   belongs_to :location
   has_many :sakes
   
+  validates :japanese_name, presence: true
+  validates :hiragana_name, presence: true
+  validates :romanized_name, presence: true
+  validates :location_id, presence: true
+  
   def localized_name
     if I18n.locale == :ja
       "#{japanese_name} (#{hiragana_name})"
