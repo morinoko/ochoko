8e4900bf6934f7e55b1b4bdb7f23d0c55c7fd83f
Felice Forby <forby.1@gmail.com>
2019-01-11 09:04:03 +0900

    implement edit and update for breweries

--- app/controllers/breweries_controller.rb
+++ app/controllers/breweries_controller.rb
@@ -1,37 +1,53 @@
 class BreweriesController < ApplicationController
-  before_action :set_brewery, only: [:show]
-  
+  before_action :set_brewery, only: [:show, :edit, :update]
+  before_action :set_locations, only: [:new, :edit]
+
   def index
     @locations = Location.all
     @breweries = Brewery.all
   end
-  
+
   def show
   end
   
   def new
     @brewery = Brewery.new
-    @locations = Location.all
   end
-  
+
   def create
     @brewery = Brewery.new(brewery_params)
     
     if @brewery.save
       redirect_to @brewery
     else
-      @locations = Location.all
+      set_locations
       render :new
     end
   end
-  
+
+  def edit
+  end
+
+  def update
+    if @brewery.update(brewery_params)
+      redirect_to @brewery
+    else
+      set_locations
+      render :edit
+    end
+  end
+
   private
   
   def brewery_params
     params.require(:brewery).permit(:japanese_name, :hiragana_name, :romanized_name, :location_id)
   end
-  
+
   def set_brewery
     @brewery = Brewery.find_by(id: params[:id])
+  end
+
+  def set_locations
+    @locations = Location.all
   end
 end
