167b61ff33b3f2eb56f693842736b33b0ccc9482
Felice Forby <forby.1@gmail.com>
2019-04-05 08:38:35 +0900

    show brewery sakes dynamically with AJAX

--- app/assets/javascripts/breweries.js
+++ app/assets/javascripts/breweries.js
@@ -1,5 +1,6 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+
 $(document).ready(function(){
   if ($('body').is('.breweries')) {
     function Brewery(breweryJSON) {
@@ -11,17 +12,24 @@ $(document).ready(function(){
       this.localizedLocation = breweryJSON.localized_location;
       this.previousId = breweryJSON.previous_id;
       this.nextId = breweryJSON.next_id;
+      this.sakes = breweryJSON.sakes;
     }
     
     if ($('body').is('.breweries.index')) {
       Brewery.prototype.templateSource = $('#brewery-template').html();
     } else if ($('body').is('.breweries.show')) {
       Brewery.prototype.templateSource = $('#brewery-show-template').html();
+      Brewery.prototype.sakeTemplateSource = $('#brewery-sake-template').html();
     }
     
     Brewery.prototype.template = Handlebars.compile(Brewery.prototype.templateSource);
     Brewery.prototype.renderHtml = function() {
       return Brewery.prototype.template(this);
+    }
+    
+    Brewery.prototype.sakeTemplate = Handlebars.compile(Brewery.prototype.sakeTemplateSource);
+    Brewery.prototype.renderHmtlForSake = function(sake) {
+      return Brewery.prototype.sakeTemplate(sake);
     }
     
     if ($('body').is('.breweries.index')) {
@@ -51,13 +59,27 @@ $(document).ready(function(){
       
       function getData(event) {
         event.preventDefault();
+        
+        $('#sake-list').empty();
+        
         let url = event.target.href;
         
         $.get(url + ".json").success(breweryData => {
           let breweryObj = new Brewery(breweryData);
           let breweryHtml = breweryObj.renderHtml();
-
+          
           $("#brewery").html(breweryHtml);
+          
+          if (breweryObj.sakes.length > 0) {
+            breweryObj.sakes.forEach(sake => {
+              let sakeHtml = breweryObj.renderHmtlForSake(sake);
+              $('#sake-list').append(sakeHtml);
+            });
+          } else {
+            const noSakes = $('#brewery-no-sakes').html();
+            $('#sake-list').append(noSakes);
+          }
+          
           attachEventListeners();
           history.pushState({brewery: breweryObj.id}, "brewery", breweryObj.id);
         }).error(error => console.log(error));
