db3c03ade61f966b879c6bd4583fb495b2fd18d2
Felice Forby <forby.1@gmail.com>
2019-04-05 06:28:49 +0900

    refactor serializer

--- app/serializers/brewery_serializer.rb
+++ app/serializers/brewery_serializer.rb
@@ -1,3 +1,11 @@
 class BrewerySerializer < ActiveModel::Serializer
-  attributes :id, :localized_name, :romanized_name, :hiragana_name, :japanese_name, :localized_location
+  attributes :id, :localized_name, :romanized_name, :hiragana_name, :japanese_name, :localized_location, :next_id, :previous_id
+  
+  def next_id
+    self.object.next ?  self.object.next.id : nil
+  end
+  
+  def previous_id
+    self.object.previous ? self.object.previous.id : nil
+  end
 end
